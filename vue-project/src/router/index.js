0603667ddb7b0672f6c0838b4211780063fe89e9
SimoLomba53 <simolomba253@gmail.com>
2023-07-23 21:49:08 +0200

    edo

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -2,14 +2,10 @@ import { createRouter, createWebHistory } from "vue-router";
 
 
 import Homepage from '../pages/Homepage.vue'
-import ProjectList from '../pages/ProjectList.vue'
 import Info from '../pages/Info.vue'
 import Tagli from '../pages/Tagli.vue'
 import ShopPage from '../pages/ShopPage.vue'
-import Thirdproject from '../pages/Thirdproject.vue'
-import Fourthproject from '../pages/Fourthproject.vue'
-import Fifthproject from '../pages/Fifthproject.vue'
-import Sixthproject from '../pages/Sixthproject.vue'
+
 
 
 const router = createRouter({
@@ -22,11 +18,7 @@ const router = createRouter({
       name: "Homepage",
       component: Homepage,
     },
-    {
-      path: "/projectlist",
-      name: "ProjectList",
-      component: ProjectList,
-    },
+   
      {
       path: "/info",
       name: "Info",
@@ -43,26 +35,7 @@ const router = createRouter({
       name: "ShopPage",
       component: ShopPage,
     },
-    {
-      path: "/thirdproject",
-      name: "Thirdproject",
-      component: Thirdproject,
-    },
-    {
-      path: "/fourthproject",
-      name: "Fourthproject",
-      component: Fourthproject,
-    },
-    {
-      path: "/fifthproject",
-      name: "Fifthproject",
-      component: Fifthproject,
-    },
-    {
-      path: "/sixthproject",
-      name: "Sixthproject",
-      component: Sixthproject,
-    },
+    
   ],
 });
 
